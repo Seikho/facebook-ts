0e2bfbb6ee929cc90e52cc35dd92b176ea3ec1c9
Carl <carl@carlossus.com>
2015-09-24 19:22:49 +0800

    Implement export api and bump version to 0.2.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+var accessToken = require('./api/accessToken');
+var getUser = require('./api/getUser');
+var settings = require('./api/settings');
+var verifyToken = require('./api/verifyToken');
+module.exports = {
+    settings: settings,
+    getUser: getUser,
+    accessToken: accessToken,
+    verifyToken: verifyToken
+};
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import accessToken = require('./api/accessToken');
+import getUser = require('./api/getUser');
+import settings = require('./api/settings');
+import verifyToken = require('./api/verifyToken');
+
+export = {
+    settings,
+    getUser,
+    accessToken,
+    verifyToken
+}
